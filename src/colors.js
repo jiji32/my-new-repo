59ceeae510b94066d5450b045f2701e4a50c1ad1
郭雅丽 <15534386+guo-yalijiji325@user.noreply.gitee.com>
2025-04-09 08:32:07 +0800

    修改背景颜色并更新依赖

--- src/colors.js
+++ src/colors.js
@@ -1,5 +1,5 @@
 export const gradientBgBase = 'bg-gradient-to-tr'
-export const gradientBgPurplePink = `${gradientBgBase} from-purple-400 via-pink-500 to-red-500`
+export const gradientBgPurplePink = `${gradientBgBase} from-pink-400 via-blue-500 to-green-500`
 export const gradientBgDark = `${gradientBgBase} from-slate-700 via-slate-900 to-slate-800`
 export const gradientBgPinkRed = `${gradientBgBase} from-pink-400 via-red-500 to-yellow-500`
 
